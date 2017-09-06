abf544ffcade5727f45844f5835827da2624989d
plumovic <student@plumovic.local>
2017-09-06 12:06:56 -0500

    Initial Commit

---
+++ Top-Stories/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Top-Stories
+//
+//  Created by plumovic on 9/6/17.
+//  Copyright © 2017 plumovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
